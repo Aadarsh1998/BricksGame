28be7ed883405c9470b1c9eb274ee27a7b306d68
DESKTOP-Aadarsh <aadarshkomera@gmail.com>
2021-02-19 20:23:30 +0530

    Level1 completed

--- Assets/BallCollision.cs
+++ Assets/BallCollision.cs
@@ -1,25 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BallCollision : MonoBehaviour
 {
     Rigidbody2D rb;
     public float ballVelocity;
-    // Start is called before the first frame update
+    public Transform paddle;
+    public float offset;
+    bool start = false;
+    public float count = 8;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Paddle")
+        if (collision.gameObject.tag == "Paddle")
         {
             //rb.velocity = new Vector2(0,10)*ballVelocity;
             print("Paddle collision");
@@ -27,15 +28,42 @@ public class BallCollision : MonoBehaviour
         else if (collision.gameObject.tag == "Boundary")
         {
             //rb.velocity = Vector2.up;
-           //rb.velocity = new Vector2(0, 10) * ballVelocity;
+            //rb.velocity = new Vector2(0, 10) * ballVelocity;
             print("Boundary collision");
+        }
+        else if(collision.gameObject.tag == "Brick")
+            {
+            Destroy(collision.gameObject);
+            count--;
+        }
+        if(count<=0)
+        {
+            print("Game over");
+            SceneManager.LoadScene(1);
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+        if (collision.gameObject.tag == "Enemy")
         {
             Destroy(gameObject);
+            SceneManager.LoadScene(1);
+            print("Ball destroyed");
+        }
+    }
+
+    void Update()
+    {
+        //transform.position = new Vector2(paddle.position.x, paddle.position.y + offset);
+        if (Input.GetMouseButtonDown(0) && start == false)
+        {
+            rb.velocity = new Vector2(0, ballVelocity);
+            start = true;
+            print("Ball launched");
+        }
+        if (start == false)
+        {
+            transform.position = new Vector2(paddle.position.x, paddle.position.y + offset);
         }
     }
 }
